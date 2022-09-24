e8c87d58bdd07a2113cb35797bb8cc7b69f7ae10
chrisachinga <chris.achinga@student.moringaschool.com>
2022-09-24 17:26:35 +0300

    done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,48 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 function Form(props) {
-  const [firstName, setFirstName] = useState("Sylvia");
-  const [lastName, setLastName] = useState("Woods");
+  const [firstName, setFirstName] = useState('Linda')
+  const [lastName, setLastName] = useState('Ndanyi')
+  const [submittedData, setSubmittedData] = useState([])
 
   function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
+    setFirstName(event.target.value)
   }
 
   function handleLastNameChange(event) {
-    setLastName(event.target.value);
+    setLastName(event.target.value)
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const formData = {
+      firstName: firstName,
+      lastName: lastName,
+    }
+    const dataArray = [...submittedData, formData]
+    setSubmittedData(dataArray)
+    setFirstName('')
+    setLastName('')
+  }
+
+  const listOfSubmissions = submittedData.map((data, index) => {
+    return (
+      <div key={index}>
+        {data.firstName} {data.lastName}
+      </div>
+    )
+  })
+
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
-  );
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type='text' onChange={handleFirstNameChange} value={firstName} />
+        <input type='text' onChange={handleLastNameChange} value={lastName} />
+        <button type='submit'>Submit</button>
+      </form>
+      <h3>{listOfSubmissions}</h3>
+    </>
+  )
 }
 
-export default Form;
+export default Form
